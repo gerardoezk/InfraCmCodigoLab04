7cd1a355517d06c3977f8b820ef83d0a19906c75
gerardoezk <gerardo.ezk.080204@gmail.com>
2025-09-27 10:14:15 -0500

    main.tf

--- Ansible + NGINX/IAC-SEM5/ia c/main.tf
+++ Ansible + NGINX/IAC-SEM5/ia c/main.tf
@@ -1,11 +1,14 @@
-# Configuramos el proveedor de Docker
+# Configuración principal de Terraform
 terraform {
+  # Declaramos qué proveedores se van a utilizar
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0"
+      source  = "kreuzwerker/docker" # Proveedor oficial para gestionar Docker
+      version = "~> 3.0"             # Versión compatible (cualquier 3.x)
     }
   }
 }
 
+# Definimos el proveedor Docker
+# Esto permite a Terraform comunicarse con el demonio de Docker
 provider "docker" {}
