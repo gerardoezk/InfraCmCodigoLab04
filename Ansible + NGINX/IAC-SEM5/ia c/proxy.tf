1bedca65b75fa5713a4a55678a42e64118035ba4
bel001 <115364491+bel001@users.noreply.github.com>
2025-09-27 10:23:09 -0500

    proxy.tf

--- Ansible + NGINX/IAC-SEM5/ia c/proxy.tf
+++ Ansible + NGINX/IAC-SEM5/ia c/proxy.tf
@@ -1,24 +1,31 @@
+# Define y descarga una imagen de Docker específica de NGINX.
 resource "docker_image" "nginx" {
-  name         = "nginx:stable-alpine3.21-perl"
-  keep_locally = false
+  name         = "nginx:stable-alpine3.21-perl"    # Nombre y tag de la imagen a obtener desde un registro
+  keep_locally = false    # Si 'false', la imagen se elimina localmente si ya no es usada por Terraform
 }
 
+# Crea y gestiona un contenedor Docker a partir de la imagen anterior
 resource "docker_container" "proxy" {  
-  name  = "proxy"
-  image = docker_image.nginx.image_id
-  
+  name  = "proxy"    # Nombre que se le dará al contenedor
+  image = docker_image.nginx.image_id    # Asocia el contenedor con la imagen de NGINX descargada previamente
+
+# Conecta el contenedor a una red Docker gestionada por Terraform
   networks_advanced {
     name = docker_network.app_net.name
   }
 
+  # Mapea el puerto 8080 de la máquina host al puerto 80 del contenedor.
   ports {
     internal = 80
     external = 8080
   }
 
+# Monta un volumen para persistir o proporcionar archivos de configuración.
+  volumes
   volumes {
-    host_path      = abspath("${path.module}/../host_volumes/nginx_conf")
-    container_path = "/etc/nginx/conf.d"
-    read_only      = false
+    host_path      = abspath("${path.module}/../host_volumes/nginx_conf")    # Ruta de la carpeta en la máquina host
+    container_path = "/etc/nginx/conf.d"    # Ruta donde se montará la carpeta dentro del contenedor
+    read_only      = false    # Permite que el contenedor escriba en el volumen
   }
+
 }
