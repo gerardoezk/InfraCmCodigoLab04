7178efcb5aabe5976f384826db0b2f0ca77f106a
gerardoezk <gerardo.ezk.080204@gmail.com>
2025-09-27 10:16:55 -0500

    nginx.tf

--- Ansible + NGINX/IAC-SEM5/ia c/nginx.tf
+++ Ansible + NGINX/IAC-SEM5/ia c/nginx.tf
@@ -1,30 +1,32 @@
+# Contenedor para la aplicación web 1
 resource "docker_container" "app1" {  
-  name  = "app1"
-  image = docker_image.nginx.image_id
+  name  = "app1"                          # Nombre del contenedor
+  image = docker_image.nginx.image_id     # Imagen base: NGINX
   
+  # Conexión del contenedor a las redes
   networks_advanced {
-    name = docker_network.app_net.name
+    name = docker_network.app_net.name     # Red de aplicación
   }
-  
   networks_advanced {
-    name = docker_network.persistence_net.name
+    name = docker_network.persistence_net.name # Red de persistencia
   }
 
+  # Montaje de volumen: conecta la carpeta host_volumes/web1 al directorio HTML de NGINX
   volumes {
-    host_path      = abspath("${path.module}/../host_volumes/web1")
-    container_path = "/usr/share/nginx/html"
-    read_only      = false
+    host_path      = abspath("${path.module}/../host_volumes/web1") # Ruta en el host
+    container_path = "/usr/share/nginx/html"                        # Ruta dentro del contenedor
+    read_only      = false                                          # Permite escritura
   }
 }
 
+# Contenedor para la aplicación web 2
 resource "docker_container" "app2" {  
   name  = "app2"
   image = docker_image.nginx.image_id
-  
+
   networks_advanced {
     name = docker_network.app_net.name
   }
-  
   networks_advanced {
     name = docker_network.persistence_net.name
   }
@@ -36,14 +38,14 @@ resource "docker_container" "app2" {
   }
 }
 
+# Contenedor para la aplicación web 3
 resource "docker_container" "app3" {  
   name  = "app3"
   image = docker_image.nginx.image_id
-  
+
   networks_advanced {
     name = docker_network.app_net.name
   }
-  
   networks_advanced {
     name = docker_network.persistence_net.name
   }
