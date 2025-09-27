5dd0a8542fad850353f41c184900cbf7e510a2d9
gerardoezk <gerardo.ezk.080204@gmail.com>
2025-09-27 10:17:27 -0500

    postgre.tf

--- Ansible + NGINX/IAC-SEM5/ia c/postgre.tf
+++ Ansible + NGINX/IAC-SEM5/ia c/postgre.tf
@@ -1,23 +1,28 @@
+# Recurso que descarga la imagen oficial de PostgreSQL versión 15 (ligera, basada en Alpine)
 resource "docker_image" "postgres" {
   name = "postgres:15-alpine"
 }
 
+# Recurso que despliega un contenedor con PostgreSQL
 resource "docker_container" "postgres" {
-  image = docker_image.postgres.image_id
-  name  = "postgres-${terraform.workspace}"
-  
+  image = docker_image.postgres.image_id          # Usa la imagen descargada arriba
+  name  = "postgres-${terraform.workspace}"       # Nombre dinámico (según workspace de Terraform)
+
+  # Conexión a la red de persistencia
   networks_advanced {
     name = docker_network.persistence_net.name
   }
   
+  # Exposición del puerto estándar de PostgreSQL
   ports {
-    internal = 5432
-    external = 5432
+    internal = 5432    # Puerto interno dentro del contenedor
+    external = 5432    # Puerto externo accesible desde el host
   }
 
+  # Variables de entorno para configurar la base de datos
   env = [
-    "POSTGRES_DB=myapp",
-    "POSTGRES_USER=postgres",
-    "POSTGRES_PASSWORD=password"
+    "POSTGRES_DB=myapp",         # Nombre de la base de datos inicial
+    "POSTGRES_USER=postgres",    # Usuario administrador
+    "POSTGRES_PASSWORD=password" # Contraseña del usuario administrador
   ]
 }
